349d15102c0f047ca88dceb55e4fd9c577e0d98f
Kenan Sevindik <kenan.sevindik@trendyol.com>
2025-10-22 14:33:50 +0300

    changes made

---
+++ examples/example-grpc/src/test/kotlin/com/fit4j/examples/grpc/AnotherGrpcExampletWithDeclarativeFixtureFIT.kt
@@ -0,0 +1,30 @@
+package com.fit4j.examples.grpc
+
+import com.example.CreditServiceGrpc
+import com.example.CreditServiceOuterClass
+import com.fit4j.annotation.FIT
+import net.devh.boot.grpc.client.inject.GrpcClient
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+@FIT
+class AnotherGrpcExampletWithDeclarativeFixtureFIT  {
+
+    @GrpcClient("inProcess")
+    private lateinit var creditServiceBlockingStub: CreditServiceGrpc.CreditServiceBlockingStub
+
+    @Test
+    fun `should get refund refund info with transaction id 123 since there is a default response defined in the fixture`() {
+        val actualResponse = creditServiceBlockingStub.getInfo(
+            CreditServiceOuterClass.GetInfoRequest.newBuilder().setRefundRef("666").build())
+
+        val expectedResponse = CreditServiceOuterClass.GetInfoResponse.newBuilder()
+            .setNewRefundId(789)
+            .build()
+
+        Assertions.assertEquals(expectedResponse, actualResponse)
+    }
+
+}
+
+
