fdc443a14ded32f21ada254927284231fb7038ba
Kenan Sevindik <kenan.sevindik@trendyol.com>
2025-10-30 18:06:08 +0300

    fixed test failure

--- fit4j-examples/example-rest/src/test/kotlin/com/fit4j/examples/rest/HttpConfig.kt
+++ fit4j-examples/example-rest/src/test/kotlin/com/fit4j/examples/rest/HttpConfig.kt
@@ -1,17 +1,22 @@
 package com.fit4j.examples.rest
 
 import okhttp3.OkHttpClient
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.ConfigurableEnvironment
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-@EnableConfigurationProperties(HttpProperties::class)
+//@EnableConfigurationProperties(HttpProperties::class)
 @Configuration
 class HttpConfig {
     @Bean
     fun createExampleRestClient(retrofit: Retrofit) = retrofit.create(ExampleRestClient::class.java)
+
+    @Autowired
+    private lateinit var env: ConfigurableEnvironment
 
     @Bean
     fun okHttpExampleRestClient(): OkHttpClient {
@@ -21,7 +26,10 @@ class HttpConfig {
     }
 
     @Bean
-    fun retrofit(okHttpExampleRestClient: OkHttpClient, httpProperties: HttpProperties): Retrofit {
+    fun retrofit(okHttpExampleRestClient: OkHttpClient): Retrofit {
+        val httpProperties = HttpProperties(
+            hostname = env.getProperty("fit4j.mockWebServer.hostName")!!,
+            port = env.getProperty("fit4j.mockWebServer.port")!!.toInt())
         return Retrofit.Builder()
             .baseUrl("${httpProperties.protocol}://${httpProperties.hostname}:${httpProperties.port}")
             .addConverterFactory(GsonConverterFactory.create())
--- fit4j-examples/example-rest/src/test/kotlin/com/fit4j/examples/rest/HttpProperties.kt
+++ fit4j-examples/example-rest/src/test/kotlin/com/fit4j/examples/rest/HttpProperties.kt
@@ -2,6 +2,6 @@ package com.fit4j.examples.rest
 
 import org.springframework.boot.context.properties.ConfigurationProperties
 
-@ConfigurationProperties("externalService")
+//@ConfigurationProperties("externalService")
 data class HttpProperties(val protocol:String="http", val hostname:String="localhost", val port:Int=8080) {
 }
